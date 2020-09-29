804db54bd720126f906a014dc30e30fcf3db64ec
NikMourelatos <nikolas@mourelatos.net>
2020-09-28 22:11:45 -0400

    first commit-Trey is dumb

---
+++ QuarantineBot/CommandHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Reflection;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace QuarantineBot
+{
+    public class CommandHandler
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _service;
+
+        // Retrieve client and CommandService instance via ctor
+        public CommandHandler(DiscordSocketClient client)//, CommandService service)
+        {
+           
+            _client = client;
+
+            _service = new CommandService();
+
+            //_service.AddModuleAsync(Assembly.GetEntryAssembly());
+            _client.MessageReceived += HandleCommandAsync;
+
+        }
+
+        
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            // Don't process the command if it was a system message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+
+            // Create a WebSocket-based command context based on the message
+            var context = new SocketCommandContext(_client, message);
+
+            // Determine if the message is a command based on the prefix and make sure no bots trigger commands
+            if (message.HasCharPrefix('^', ref argPos)) {
+                // var result = await _service.ExecuteAsync(context,argPos,_service);
+
+                //if ((!result.IsSuccess) && (result.Error != CommandError.UnknownCommand))
+                //{
+                   await context.Channel.SendMessageAsync("Trey is dumb");
+                //}
+            }
+                
+                //return;
+
+            
+            await message.Channel.SendMessageAsync("Trey is Dumb");
+
+            // Execute the command with the command context we just
+            // created, along with the service provider for precondition checks.
+            //await _commands.ExecuteAsync(
+              //  context: context,
+                //argPos: argPos,
+                //services: null);
+        }
+    }
+}
---
+++ QuarantineBot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Discord.WebSocket;
+using Discord.Commands;
+using Discord;
+
+namespace QuarantineBot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+
+        private DiscordSocketClient _client;
+        private CommandHandler _handler;
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            
+
+            _client.Log += Log;
+
+            var token = "NzYwMjkwNTY1NDEzMjczNjMw.X3J5xA.JiwJsWNC1e10n3X_jC-1HthBQCk";
+
+            // var token = File.ReadAllText("token.txt");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+
+            _handler = new CommandHandler(_client);
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
